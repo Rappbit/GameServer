ba3e8fb98a8e96c1d90705958499484922485a99
Alexander Keussen <alexander.keussen@tum.de>
2020-06-11 17:07:28 +0200

    basic server working

--- game_data.py
+++ game_data.py
@@ -1,5 +1,10 @@
+import socket
+
+
 class GameData:
     value = "test"
+    clients = []
+    serverSocket = None
 
     def __init__(self):
         pass
--- game_server.py
+++ game_server.py
@@ -1,11 +1,17 @@
+#!/usr/bin/python3
 import socket
 import asyncio
+import threading
+import _thread
+import time
 from client import Client
 from game_data import GameData
+from broadcaster import Broadcaster
 
 localIP = '0.0.0.0'
 localPort = 20001
 bufferSize = 1024
+tickRate = 10
 
 # Create datagram socket
 serverSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
@@ -13,31 +19,40 @@ serverSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 shouldListen = True
 shouldBroadcast = True
 
-clients = []
+gameData = GameData()
 
 
 def main():
     # Bind to address and ip
     serverSocket.bind((localIP, localPort))
-
+    gameData.serverSocket = serverSocket
     print("UDP Server up and listening")
 
-    # Create Tick Timer
-    tickLoop = asyncio.get_event_loop()
-    tickListenTask = tickLoop.create_task(tickListen())
-    tickBroadcastTask = tickLoop.create_task(tickBroadcast())
-
-    try:
-        tickLoop.run_until_complete(tickListenTask)
-        tickLoop.run_until_complete(tickBroadcastTask)
-    except asyncio.CancelledError:
+    #listen()
+   # broadcaster = Broadcaster(1, "Broadcast")
+    #broadcaster.gameData = gameData
+   # broadcaster.start()
+    _thread.start_new_thread(listen, ())
+    _thread.start_new_thread(broadcastTick(), ())
+    while 1:
         pass
+    # Create Tick Timer
+    # tickLoop = asyncio.get_event_loop()
+    # tickListenTask = tickLoop.create_task(tickListen())
+    # tickBroadcastTask = tickLoop.create_task(tickBroadcast())
+
+    # try:
+    # tickLoop.run_until_complete(tickListenTask)
+    # tickLoop.run_until_complete(tickBroadcastTask)
+
+
+# except asyncio.CancelledError:
+# pass
 
 
 def listen():
-
     # Listen for incoming datagrams
-    if shouldListen:
+    while shouldListen:
         bytesAddressPair = serverSocket.recvfrom(bufferSize)
         message = bytesAddressPair[0].decode().strip()
         address = bytesAddressPair[1]
@@ -49,32 +64,38 @@ def listen():
 
 
 def parseMessage(msg, address):
-    if msg == "connect" and address not in clients:
+    print("parsing message: {}".format(msg))
+    if msg == "connect" and address not in gameData.clients:
         print ("client {} connected".format(address))
-        clients.append(Client(address))
+        gameData.clients.append(Client(address))
 
 
-async def tickListen():
-    print("Listening")
-    while True:
-        listen()
-        await asyncio.sleep(0.1)
+# async def tickListen():
+#     print("Listening")
+#     while True:
+#         listen()
+#         await asyncio.sleep(0.1)
+#
+#
+# async def tickBroadcast():
+#     print("fasfa")
+#     while True:
+#         broadcast("test")
+#         await asyncio.sleep(0.1)
 
-
-async def tickBroadcast():
-    print("fasfa")
-    while True:
-        broadcast("test")
-        await asyncio.sleep(0.1)
+def broadcastTick():
+    msg = "Alex"
+    while shouldBroadcast:
+        broadcast(msg)
+        time.sleep(1.0 / tickRate)
 
 
 def broadcast(msg):
-    print("broadcast")
-    if shouldBroadcast:
-        msg = msg.encode()
-        for client in clients:
-            address = client.address
-            serverSocket.sendto(msg, address)
+    print ("broadcasting: {}".format(msg))
+    msg = msg.encode()
+    for client in gameData.clients:
+        address = client.address
+        serverSocket.sendto(msg, address)
 
 
 if __name__ == "__main__":
